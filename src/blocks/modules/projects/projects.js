03127eb8cdabfa1edec30cb2559dc9ad6a7441dc
Viktoria <uletela@gmail.com>
2023-04-24 14:58:05 +0300

    bug fix

--- src/blocks/modules/projects/projects.js
+++ src/blocks/modules/projects/projects.js
@@ -6,8 +6,9 @@ import Swiper, { Navigation, Pagination } from 'swiper';
 
 new Swiper(".projects_swiper", {
     speed: 800,
-    autoHeight: true,
+    // autoHeight: true,
     simulateTouch: true,
+    slidesPerView: 'auto',
     modules: [Navigation, Pagination],
     pagination: {
         el: '.swiper-pagination',
@@ -17,7 +18,7 @@ new Swiper(".projects_swiper", {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
-    breakpoints: {
+    /* breakpoints: {
         360: {
             slidesPerView: 1,
             spaceBetween: 30,
@@ -35,15 +36,16 @@ new Swiper(".projects_swiper", {
 
 
         },
-    }
+    } */
 
 });
 
 
 new Swiper(".similar_swiper", {
     speed: 800,
-    autoHeight: true,
+    // autoHeight: true,
     simulateTouch: true,
+    slidesPerView: 'auto',
     modules: [Navigation, Pagination],
     pagination: {
         el: '.swiper-pagination',
@@ -53,24 +55,24 @@ new Swiper(".similar_swiper", {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
-    breakpoints: {
-        360: {
-            slidesPerView: 1,
-            spaceBetween: 30,
+    // breakpoints: {
+    //     360: {
+    //         slidesPerView: 1,
+    //         spaceBetween: 30,
 
-        },
-        480: {
-            slidesPerView: 2,
-            spaceBetween: 30,
+    //     },
+    //     480: {
+    //         slidesPerView: 2,
+    //         spaceBetween: 30,
 
-        },
+    //     },
 
-        680: {
-            slidesPerView: 3,
-            spaceBetween: 46,
+    //     680: {
+    //         slidesPerView: 3,
+    //         spaceBetween: 46,
 
 
-        },
-    }
+    //     },
+    // }
 
 });
