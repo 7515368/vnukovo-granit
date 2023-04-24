03a890babe68e52038db69dde4cf77c0d79d12fd
Viktoria <uletela@gmail.com>
2023-04-24 16:24:52 +0300

    accordion change

--- src/blocks/modules/pages/pages.js
+++ src/blocks/modules/pages/pages.js
@@ -76,8 +76,8 @@ const items = document.querySelectorAll(".accordion button");
 function toggleAccordion() {
     const itemToggle = this.getAttribute('aria-expanded');
 
-    for (i = 0; i < items.length; i++) {
-        items[i].setAttribute('aria-expanded', 'false');
+    for (const element of items) {
+        element.setAttribute('aria-expanded', 'false');
     }
 
     if (itemToggle == 'false') {
